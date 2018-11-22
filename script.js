f39af5f77714c1f13ee4e9a481ef433d9c85b59e
Gergely Acs <gergely.acs89@gmail.com>
2018-11-22 23:44:27 +0100

    Window automatic sizing development started.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $( document ).ready(function() {
 	setConsts();
 
+	rethinkPanels();
+
 	$("body").append(createMap(g.cols, g.rows));
 	setTimeout(later,1800);
 	$(".cell").click(clicked);
 	$(".cell").attr("clicked",0);
 	
-	rethinkPanels();
 	
 	g.resizeTimeout = null;
 	$( window ).resize(function() {
@@ -21,9 +22,13 @@ $( document ).ready(function() {
 function setConsts()
 {
 	g = {};
-	g.rows = 5;
-	g.cols = 5;
+	g.sceneRows = 20;
+	g.sceneCols = 30;
 	g.dAspectRatio = 4/3;
+	g.gapRatio = 1/6;
+	g.actualCellSize = 40; 	// px
+	g.minCellSize = 30; 		// px
+	g.stepCellSize = 5; 		// px
 }
 
 function later()
@@ -78,6 +83,8 @@ function rethinkPanels()
 	calcMapSize();
 	
 
+	g.cols = 5; // tmp
+	g.rows = 5; // tmp
 	console.log(g);
 }
 
@@ -107,5 +114,44 @@ function calcDashboardSize()
 
 function calcMapSize()
 {
+	g.mShort = g.wShort;
+	g.mLong = g.wLong - g.dLong;
+	
+	g.mShorterSide = ( g.mShort < g.mLong ? g.mShort : g.mLong );
+	// n*cell.width+(n+1)*gap =g.mShorterSide
+	// cell.width * gapRatio =  gap
+	// n*cell.width+ (n+1)*cell.width * gapRatio =g.mShorterSide
+	//cell.width * (n+ (n+1)* gapRatio = g.mShorterSide 
+	
+	calcCellNum(); 
+}
+
+function calcCellNum()
+{
+	// ha eleg nagy a terulet --> mindent kirajzol actual merettel (g.actualCellSize)
+	// ha nem                 --> g.actualCellSize = mShort/ cell.width
+	
+	var currentTableSize = 0; //tmp
+	if (true)   // if kifer az osszes
+	{
+		// upscale
+	}
+	else  // csak resz-scene lesz mutatva
+	{
+		if (true)  // legalabb 3 kifer
+		{
+			// megmutatjuk a kiszamolt meretben
+		}
+		else  // nem fer ki 3
+		{
+			// de, 3, lemegyunk a min. meret ala
+		}
+	}
 
 }
+
+function getMapSize(cellSize)
+{
+	var gap = Math.floor(cellSize * g.gapRatio);
+	var mapSize = 0; //tmp
+	}
